c49d88dcd04af737e0fc8ffc4de279f9ce5c1a79
Insomnia <282132315@qq.com>
2019-04-25 22:17:10 +0800

    add admin html && css

---
+++ src/components/sideMenu.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="side-menu-wrapper">
+    <div @click="goTo('manage')">
+      <Icon class="side-menu-wrapper-item" size="36" type="md-cash"/>
+    </div>
+    <div @click="goTo('add')">
+      <Icon class="side-menu-wrapper-item" size="36" type="md-add"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "sideMenu",
+    methods: {
+      goTo(path) {
+        if (path === 'manage') {
+          this.$router.push('/admin/manage')
+        } else if (path === 'add') {
+          this.$router.push('/admin/add')
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .side-menu-wrapper {
+    height: 800px;
+    background-color: #565f6d;
+    width: 50px;
+
+    display: flex;
+    flex-direction: column;
+  }
+
+  .side-menu-wrapper-item {
+    height: 50px;
+    color: white;
+    margin-top: 10px;
+    padding: 5px;
+  }
+</style>
