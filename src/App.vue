fc1f27686a33836f842736a1efbc18091d712eb1
Insofan <282132315@qq.com>
2019-05-08 15:38:03 +0800

    gtmdzj init

--- src/App.vue
+++ src/App.vue
@@ -1,76 +1,76 @@
 <template>
-  <div class="layout">
-    <Layout>
-      <Header>
-        <Menu mode="horizontal" theme="dark" active-name="1">
-          <div class="top-bar">
-            <div class="logo-top-left"></div>
-            <div>
-              <MenuItem>
-                <router-link to="/">主页</router-link>
-              </MenuItem>
-              <MenuItem>
-                <router-link to="/about">赛事</router-link>
-              </MenuItem>
-              <MenuItem>韦恩大厦</MenuItem>
-            </div>
-          </div>
-        </Menu>
-      </Header>
-      <Content>
-        <router-view/>
-      </Content>
-    </Layout>
-  </div>
+	<div class="layout">
+		<Layout>
+			<Header>
+				<Menu mode="horizontal" theme="dark" active-name="1">
+					<div class="top-bar">
+						<router-link class="logo-top-left" to="/"></router-link>
+						<div>
+							<MenuItem name="1">
+								<router-link to="/">主页</router-link>
+							</MenuItem>
+							<MenuItem name="2">
+								<router-link to="/about">赛事</router-link>
+							</MenuItem>
+							<MenuItem name="3">韦恩大厦</MenuItem>
+						</div>
+					</div>
+				</Menu>
+			</Header>
+			<Content>
+				<router-view/>
+			</Content>
+		</Layout>
+	</div>
 </template>
 
 <style>
-html,
-body,
-.ivu-layout {
-  width: 100%;
-  height: 100%;
-}
+	html,
+	body,
+	.ivu-layout {
+		width: 100%;
+		height: 100%;
+	}
 
-.ivu-layout-content {
-  overflow: auto;
-}
+	.ivu-layout-content {
+		overflow: auto;
+	}
 
-.ivu-layout-header,
-.ivu-menu-dark {
-  background: #001529;
-}
+	.ivu-layout-header,
+	.ivu-menu-dark {
+		background: #001529;
+	}
 
-.ivu-menu-horizontal {
-  height: 100%!important;
-}
+	.ivu-menu-horizontal {
+		height: 100%!important;
+	}
 
-.layout{
-  background: #f5f7f9;
-  position: relative;
-  border-radius: 4px;
-  overflow: hidden;
-  height: 100%;
-}
+	.layout{
+		background: #f5f7f9;
+		position: relative;
+		border-radius: 4px;
+		overflow: hidden;
+		height: 100%;
+	}
 
-.top-bar {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-}
+	.top-bar {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		width: 100%;
+		height: 100%;
+	}
 
-.logo-top-left {
-  float: left;
-  width: 100px;
-  height: 30px;
-  background: url('./assets/imgs/logo-top.png');
-  background-size: 100% 100%;
-}
+	.logo-top-left {
+		float: left;
+		width: 100px;
+		height: 30px;
+		background: url('./assets/imgs/logo-top.png');
+		background-size: 100% 100%;
+	}
 
-.home {
-  width: 100%;
-  height: 100%;
-}
+	.home {
+		width: 100%;
+		height: 100%;
+	}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,75 +1,150 @@
 <template>
-  <div class="about">
-    <div>
-      <div class="content-bg">
-        <div class="content-pic-font"></div>
-        <headPic></headPic>
-      </div>
-      <div class="content-game-title">
-        <div class="coming-game"></div>
-      </div>
-      <div class="game-content">
-        <gameList></gameList>
-      </div>
-    </div>
-  </div>
+	<div>
+		<div>
+			<div class="content-bg">
+				<div class="content-pic-font"></div>
+				<headPic></headPic>
+			</div>
+			<div class="content-game-title">
+				<div class="coming-game"></div>
+			</div>
+			<div v-for="game in gamesList" :key="game.id" class="game-content">
+				<div class="game-bg">
+					<div style="font-size: 20px">比赛编号: {{game.getId()}}</div>
+					<div class="game-bo-n">{{game.getName()}}</div>
+					<div class="game-deadline">下注截至日期:<br> {{new Date(game.getEndTimeMs())}}</div>
+					<Button @click="showBetModal()">查看详情</Button>
+					<Modal
+							v-model="isShowBetModal"
+							title="下注选项"
+					>
+						<div v-for="option in game.getBettingOptionsList()" :key="option.id">
+							<Tag :checkable="true" :checked="false" color="error" @on-change="setBetOption(option)">{{option.getName()}} 赔率: {{option.getOdds()}}</Tag>
+						</div>
+					</Modal>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script>
+<script lang="ts">
+  import Vue from "vue";
+  import Component from "vue-class-component";
+  import headPic from '../components/headPic.vue';
+  import {ApiService} from '@/common/api.service';
+  import {ListGameReply, ListGameRequest, Game} from '@/proto/bbuhot/service/game_pb'
 
-import headPic from '../components/headPic.vue';
-import gameList from '../components/gameList.vue';
-import '../common/testing';
-
-export default {
-  components: {
-    headPic,
-    gameList
-  },
-  name: 'home',
-  data() {
-    return {
+  @Component({
+    components: {
+      headPic: headPic,
     }
-  },
-}
+  })
+
+  export default class Home extends Vue {
+
+    listGameReply: ListGameReply = new ListGameReply();
+    gamesList: Array<Game> = new Array<Game>();
+    isShowBetModal: boolean = false;
+
+    // request
+    private async listRequest(): Promise<Array<Game>> {
+      const listGameRequest = new ListGameRequest();
+      listGameRequest.setGameStatus(Game.Status.PUBLISHED);
+
+      this.listGameReply = await ApiService.listGame(listGameRequest);
+
+      if (this.listGameReply.hasAuthErrorCode()) {
+        // TODO: deal with error.
+      }
+
+      return this.listGameReply.getGamesList();
+    }
+
+    private async loadGamesList() {
+      this.gamesList = await this.listRequest();
+    }
+
+    private showBetModal() {
+      this.isShowBetModal = !this.isShowBetModal;
+    }
+
+    private setBetOption(option: any){
+      console.log('set bet option', option);
+    }
+
+    async mounted() {
+      this.loadGamesList();
+    }
+
+  }
 </script>
 
 <style>
-.content-bg {
-  width: 100%;
-  height: 400px;
-  background: url('../assets/imgs/bg.png');
-  background-size: 100% 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 50px;
-}
+	.content-bg {
+		width: 100%;
+		height: 400px;
+		background: url('../assets/imgs/bg.png');
+		background-size: 100% 100%;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		padding: 0 50px;
+	}
 
-.content-pic-font {
-  width: 600px;
-  height: 160px;
-  background: url('../assets/imgs/banner.png');
-  background-size: 100% 100%;
-}
+	.content-pic-font {
+		width: 600px;
+		height: 160px;
+		background: url('../assets/imgs/banner.png');
+		background-size: 100% 100%;
+	}
 
-.coming-game {
-  width: 221px;
-  height: 66px;
-  background: url('../assets/imgs/logo.png');
-  background-size: 100% 100%;
-}
+	.coming-game {
+		width: 221px;
+		height: 66px;
+		background: url('../assets/imgs/logo.png');
+		background-size: 100% 100%;
+	}
 
-.content-game-title {
-  padding: 50px;
-}
+	.content-game-title {
+		padding: 50px;
+	}
 
-.game-content {
-  padding: 0 50px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+	.game-content {
+		padding: 0 50px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.game-bg {
+		width: 1200px;
+		height: 170px;
+		background: #fff;
+		margin-bottom: 16px;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+
+	.game-bo-n {
+		font-size: 48px;
+		font-weight: 600;
+		color: rgba(245, 51, 19, 1);
+		text-align: center;
+	}
+
+	.game-deadline {
+		width: 400px;
+		height: 68px;
+		display: flex;
+		justify-content: space-around;
+		flex-direction: column;
+		font-size: 16px;
+		font-weight: 600;
+		color: rgba(85, 85, 85, 1);
+		text-align: center;
+	}
 
 </style>
 
